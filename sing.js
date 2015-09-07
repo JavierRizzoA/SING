b8367229fb6b692323cd16972c8b278f3d97c775
Javier Rizzo <javierrizzoa@gmail.com>
2015-09-07 14:38:54 -0700

    Make basic settings interface

---
+++ sing.js
@@ -0,0 +1,39 @@
+$('#new-process-probability-slider').slider({
+    precission: 0,
+    value: 50
+});
+
+$('#quantum-slider').slider({
+    precission: 0,
+    value: 5
+});
+
+$('#delay-slider').slider({
+    precission: 2,
+    value: 0.2,
+    step: 0.01
+});
+
+$('#cores-slider').slider({
+    precission: 0,
+    value: 4
+});
+
+$('#new-limit-slider').slider({
+    precission: 0,
+    value: 50
+});
+
+$('#ready-limit-slider').slider({
+    precission: 0,
+    value: 50
+});
+
+$('#waiting-limit-slider').slider({
+    precission: 0,
+    value: 50
+});
+
+$('#cpu-slider').slider({});
+
+$('#io-slider').slider({});
